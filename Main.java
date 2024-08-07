9a98428de858ed6cc17e1fd51ae1ac034dc6779c
F04C <israeldarrel01@gmail.com>
2024-08-07 10:17:35 +0800

    test

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+
+
+
+//        int age = 30;
+//
+//        System.out.println(age);
+        Date now = new Date();
+
+        System.out.println(now);
+    }
+
+
+}
