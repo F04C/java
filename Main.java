4dc698d7522ed95af51eeccefcbf8f850deee7cd
F04C <israeldarrel01@gmail.com>
2024-08-07 10:24:53 +0800

    pointers

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package com.tutorial;
 
-import java.util.Date;
+import java.awt.*;
+//import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +12,25 @@ public class Main {
 //        int age = 30;
 //
 //        System.out.println(age);
-        Date now = new Date();
+//        Date now = new Date();
+//
+//        System.out.println(now);
 
-        System.out.println(now);
+//
+//        byte x = 1;
+//        byte y = 1;
+//
+//        x = 2;
+//
+
+
+
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+
+        System.out.println(point2);
+
     }
 
 
